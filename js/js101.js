1a62f9eca2c1d40baa3926c7d6f72c66e1dc8054
Oak Soe Kyaw <oaksoe.kyaw@bolttech.io>
2022-06-12 17:25:44 +0800

    initial js pods

---
+++ js/js101.js
@@ -0,0 +1,12 @@
+let school;
+const age = 20;
+
+if (age < 10) {
+    school = 'primary';
+} else if (age < 15) {
+    school = 'secondary';
+} else {
+    school = 'college';
+}
+
+console.log('School: ', school);
